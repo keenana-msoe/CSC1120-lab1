af231c5fa1f9a938986b772b581f9dcb664bd6bd
keenana-msoe <keenana@msoe.edu>
2024-11-10 18:14:38 -0600

    Done

---
+++ src/keenana/MeanImageMedian.java
@@ -0,0 +1,278 @@
+/*
+Course: CSC1120
+Term: Spring 2024
+Assignment: Lab 1
+Creator: Andrew Keenan
+Date: 1-17-24
+*/
+package keenana;
+
+import mocked.Image;
+import mocked.PixelReader;
+import mocked.PixelWriter;
+import mocked.WritableImage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * Calculates the mean and median of the Images
+ * plus reads incoming images and writes to files form Image objects
+ */
+public class MeanImageMedian {
+    
+    /**
+     * Maximum color value
+     */
+    public static final int MAX_COLOR = 255;
+
+    /**
+     * Calculates the median of all the images passed to the method.
+     * <br />
+     * Each pixel in the output image consists is calculated as the median
+     * red, green, and blue components of the input images at the same location.
+     * @param inputImages Images to be used as input
+     * @return An image containing the median color value for each pixel in the input images
+     *
+     * @throws IllegalArgumentException Thrown if inputImages or any element of inputImages is null,
+     * the length of the array is less than two, or  if any of the input images differ in size.
+     */
+    public static Image calculateMedianImage(Image[] inputImages) {
+        int length = inputImages.length;
+        double x = inputImages[0].getWidth();
+        double y = inputImages[0].getHeight();
+        boolean same = true;
+        for (Image i : inputImages){
+            if (i.getWidth() != x) {
+                same = false;
+            }
+            if (i.getHeight() != y){
+                same = false;
+            }
+        }
+        WritableImage median = new WritableImage((int)x, (int)y);
+        PixelWriter writer = median.getPixelWriter();
+        if (same) {
+            for (int i = 0; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    int pixel;
+                    int[] rsum = new int[length];
+                    int[] gsum = new int[length];
+                    int[] bsum = new int[length];
+                    for (int k = 0; k < length; k++) {
+                        PixelReader reader = inputImages[k].getPixelReader();
+                        int argb = reader.getArgb(j, i);
+                        rsum[k] = argbToRed(argb);
+                        gsum[k] = argbToGreen(argb);
+                        bsum[k] = argbToBlue(argb);
+                    }
+                    Arrays.sort(rsum);
+                    Arrays.sort(gsum);
+                    Arrays.sort(bsum);
+                    int red;
+                    int green;
+                    int blue;
+
+                    if (length % 2 == 0){
+                        red = (rsum[length / 2] + rsum[(length / 2)- 1]) / 2;
+                        green = (gsum[length / 2] + gsum[(length / 2)- 1]) / 2;
+                        blue = (bsum[length / 2] + bsum[(length / 2)- 1]) / 2;
+                    } else {
+                        int division = (int) (length / 2);
+                        red = rsum[division];
+                        green = gsum[division];
+                        blue = bsum[division];
+                    }
+                    pixel = argbToInt(MAX_COLOR, red, green, blue);
+                    writer.setArgb(j, i, pixel);
+                }
+            }
+        }
+        return median;
+    }
+
+    /**
+     * Calculates the mean of all the images passed to the method.
+     * <br />
+     * Each pixel in the output image consists is calculated as the average of the
+     * red, green, and blue components of the input images at the same location.
+     * @param inputImages Images to be used as input
+     * @return An image containing the mean color value for each pixel in the input images
+     *
+     * @throws IllegalArgumentException Thrown if inputImages or any element of inputImages is null,
+     * the length of the array is less than two, or  if any of the input images differ in size.
+     */
+    public static Image calculateMeanImage(Image[] inputImages) {
+        int length = inputImages.length;
+        double x = inputImages[0].getWidth();
+        double y = inputImages[0].getHeight();
+        boolean same = true;
+        for (Image i : inputImages){
+            if (i.getWidth() != x) {
+                same = false;
+            }
+            if (i.getHeight() != y){
+                same = false;
+            }
+        }
+        WritableImage mean = new WritableImage((int)x, (int)y);
+        PixelWriter writer = mean.getPixelWriter();
+        if (same) {
+            for (int i = 0; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    int pixel;
+                    int rsum = 0;
+                    int gsum = 0;
+                    int bsum = 0;
+                    for (Image inputImage : inputImages) {
+                        PixelReader reader = inputImage.getPixelReader();
+                        int argb = reader.getArgb(j, i);
+                        rsum += argbToRed(argb);
+                        gsum += argbToGreen(argb);
+                        bsum += argbToBlue(argb);
+                    }
+                    rsum = rsum / length;
+                    gsum = gsum / length;
+                    bsum = bsum / length;
+                    pixel = argbToInt(MAX_COLOR, rsum, gsum, bsum);
+                    writer.setArgb(j, i, pixel);
+                }
+            }
+        }
+        return mean;
+    }
+
+    /**
+     * Reads an image in PPM format. The method only supports the plain PPM (P3) format with 24-bit color
+     * and does not support comments in the image file.
+     * @param imagePath the path to the image to be read
+     * @return An image object containing the image read from the file.
+     *
+     * @throws IllegalArgumentException Thrown if imagePath is null.
+     * @throws IOException Thrown if the image format is invalid or there was trouble reading the file.
+     */
+    public static Image readPPMImage(Path imagePath) throws IOException {
+        Scanner in = new Scanner(imagePath);
+        WritableImage copy;
+        if (in.nextLine().equals("P3")) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            copy = new WritableImage(x, y);
+            PixelWriter writer = copy.getPixelWriter();
+            int alpha = in.nextInt();
+            if (alpha == MAX_COLOR){
+                for (int i = 0; i < y; i++) {
+                    for (int j = 0; j < x; j++) {
+                        int red = in.nextInt();
+                        int green = in.nextInt();
+                        int blue = in.nextInt();
+                        int pixel = argbToInt(alpha, red, green, blue);
+                        writer.setArgb(j, i, pixel);
+                    }
+                }
+            } else {
+                throw new IOException();
+            }
+        } else {
+            throw new IOException();
+        }
+        return copy;
+    }
+
+    /**
+     * Writes an image in PPM format. The method only supports the plain PPM (P3) format with 24-bit color
+     * and does not support comments in the image file.
+     * @param imagePath the path to where the file should be written
+     * @param image the image containing the pixels to be written to the file
+     *
+     * @throws IllegalArgumentException Thrown if imagePath is null.
+     * @throws IOException Thrown if the image format is invalid or there was trouble reading the file.
+     */
+    public static void writePPMImage(Path imagePath, Image image) throws IOException {
+        PixelReader reader = image.getPixelReader();
+        File output = imagePath.toFile();
+        PrintWriter writer = new PrintWriter(output);
+        double x = image.getWidth();
+        double y = image.getHeight();
+        writer.write("P3\n"+(int)x+" "+(int)y+"\n"+MAX_COLOR);
+        for (int i = 0; i < y; i++){
+            writer.write("\n");
+            for (int j = 0; j < x; j++){
+                int argb = reader.getArgb(j, i);
+                int red = argbToRed(argb);
+                int green = argbToGreen(argb);
+                int blue = argbToBlue(argb);
+                writer.write(red+" "+green+" "+blue+" ");
+            }
+        }
+        writer.close();
+    }
+
+    /**
+     * Extract 8-bit Alpha value of color from 32-bit representation of the color in the format
+     * described by the INT_ARGB PixelFormat type.
+     * @param argb the 32-bit representation of the color
+     * @return the 8-bit Alpha value of the color.
+     */
+    private static int argbToAlpha(int argb) {
+        final int bitShift = 24;
+        return argb >> bitShift;
+    }
+
+    /**
+     * Extract 8-bit Red value of color from 32-bit representation of the color in the format
+     * described by the INT_ARGB PixelFormat type.
+     * @param argb the 32-bit representation of the color
+     * @return the 8-bit Red value of the color.
+     */
+    private static int argbToRed(int argb) {
+        final int bitShift = 16;
+        final int mask = 0xff;
+        return (argb >> bitShift) & mask;
+    }
+
+    /**
+     * Extract 8-bit Green value of color from 32-bit representation of the color in the format
+     * described by the INT_ARGB PixelFormat type.
+     * @param argb the 32-bit representation of the color
+     * @return the 8-bit Green value of the color.
+     */
+    private static int argbToGreen(int argb) {
+        final int bitShift = 8;
+        final int mask = 0xff;
+        return (argb >> bitShift) & mask;
+    }
+
+    /**
+     * Extract 8-bit Blue value of color from 32-bit representation of the color in the format
+     * described by the INT_ARGB PixelFormat type.
+     * @param argb the 32-bit representation of the color
+     * @return the 8-bit Blue value of the color.
+     */
+    private static int argbToBlue(int argb) {
+        final int bitShift = 0;
+        final int mask = 0xff;
+        return (argb >> bitShift) & mask;
+    }
+
+    /**
+     * Converts argb components into a single int that represents the argb value of a color.
+     * @param a the 8-bit Alpha channel value of the color
+     * @param r the 8-bit Red channel value of the color
+     * @param g the 8-bit Green channel value of the color
+     * @param b the 8-bit Blue channel value of the color
+     * @return a 32-bit representation of the color in the format described by the INT_ARGB PixelFormat type.
+     */
+    private static int argbToInt(int a, int r, int g, int b) {
+        final int alphaShift = 24;
+        final int redShift = 16;
+        final int greenShift = 8;
+        final int mask = 0xff;
+        return a << alphaShift | ((r & mask) << redShift) | (g & mask) << greenShift | b & mask;
+    }
+}
